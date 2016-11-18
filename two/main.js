2f30e219599da17ce4bfaa15c56f71eda16c7745
Joel Jacob <joel@viastudio.com>
2016-11-18 13:02:45 -0500

    UI Animation example

--- two/main.js
+++ two/main.js
@@ -1,4 +1,32 @@
-$('button').on('click', animate();
-button.click(function() {
+// $.Velocity.mock = 5;
 
-});
+function animate() {
+
+    $('button').velocity({
+        width: '3.375rem',
+        borderRadius: '1.6875rem',
+        backgroundColor: '#8cc152',
+        borderColor: '#344b1b',
+        color: '#344b1b'
+    }, {
+        duration: 350,
+        easing: "easeInQuad",
+        complete: function () {
+            $('.icon').velocity({
+                opacity: 1
+            }, {
+                duration: 150
+            });
+        }
+    });
+
+    $('.text').velocity({
+        opacity: 0,
+        scale: 0
+    }, {
+        duration: 150
+    });
+};
+
+$('button').on('click', animate);
+
